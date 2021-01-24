2655b1a029c6768692744749ff3123217c2d92be
Gus Narea <gnarea@users.noreply.github.com>
2021-01-24 20:16:22 +0000

    fix(Minio): Make credentials required

--- src/lib/adapters/MinioClient.spec.ts
+++ src/lib/adapters/MinioClient.spec.ts
@@ -1,3 +1,4 @@
+import { CLIENT_CREDENTIALS, ENDPOINT } from '../_test_utils';
 import { ObjectStorageError } from '../ObjectStorageError';
 import { MinioClient } from './MinioClient';
 
@@ -6,8 +7,15 @@ jest.mock('aws-sdk');
 describe('Constructor', () => {
   test('Error should be thrown if endpoint is not set', () => {
     // tslint:disable-next-line:no-unused-expression
-    expect(() => new MinioClient({ tlsEnabled: true })).toThrow(
+    expect(() => new MinioClient({ credentials: CLIENT_CREDENTIALS, tlsEnabled: true })).toThrow(
       new ObjectStorageError('The Minio adapter requires an endpoint'),
+    );
+  });
+
+  test('Error should be thrown if credentials are not set', () => {
+    // tslint:disable-next-line:no-unused-expression
+    expect(() => new MinioClient({ endpoint: ENDPOINT, tlsEnabled: true })).toThrow(
+      new ObjectStorageError('The Minio adapter requires credentials'),
     );
   });
 });
