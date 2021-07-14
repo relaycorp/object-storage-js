5f61134f5f6087ab8306440c378facb62f8d83f6
Gus Narea <gnarea@users.noreply.github.com>
2021-07-14 11:04:11 +0100

    chore(CI): Output Docker container logs when functional test suite fails (#95)

--- src/functional_tests/run.sh
+++ src/functional_tests/run.sh
@@ -9,14 +9,15 @@ CURRENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 export COMPOSE_PROJECT_NAME='object-storage-functional-tests'
 export COMPOSE_FILE="${CURRENT_DIR}/docker-compose.yml"
 
-trap "docker-compose down --remove-orphans" INT TERM EXIT
+trap "docker-compose down --remove-orphans --volumes" INT TERM EXIT
 
 docker-compose pull
-docker-compose build
 
 docker-compose --env-file "$(pwd)/.env" up  --force-recreate  --detach
 sleep 3s
 
 docker-compose ps
 
-exec jest --config jest.config.functional.js --runInBand --detectOpenHandles
+if ! jest --config jest.config.functional.js --runInBand --detectOpenHandles ; then
+  docker-compose logs gcs
+fi
